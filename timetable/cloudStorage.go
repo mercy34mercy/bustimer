a1f4342261cd202de19dbfda72c636ce59612e86
shun-shun123 <shunsuke15vb@gmail.com>
2019-10-11 18:24:34 +0900

    google cloud storageとの接続処理のためコードをリファクタリング

---
+++ timetable/cloudStorage.go
@@ -0,0 +1,49 @@
+package timetable
+
+import (
+	"bytes"
+	"context"
+	"io"
+
+	"cloud.google.com/go/storage"
+)
+
+// fetchFromCloudStorage CloudStorageからfileNameに該当するファイルのbyteデータを取得する
+func fetchFromCloudStorage(fileName string) ([]byte, error) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return []byte{}, err
+	}
+	bkt := client.Bucket(bucket)
+	obj := bkt.Object(fileName)
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer r.Close()
+	buf := new(bytes.Buffer)
+	if _, err := io.Copy(buf, r); err != nil {
+		return []byte{}, err
+	}
+	return buf.Bytes(), nil
+}
+
+// saveToCloudStorage dataのバイト列をfileNameで示されるファイルに保存する
+func saveToCloudStorage(data []byte, fileName string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	bkt := client.Bucket(bucket)
+	obj := bkt.Object(fileName)
+	w := obj.NewWriter(ctx)
+	if _, err := w.Write(data); err != nil {
+		return err
+	}
+	if err := w.Close(); err != nil {
+		return err
+	}
+	return nil
+}
--- timetable/timetable.go
+++ timetable/timetable.go
@@ -1,17 +1,14 @@
 package timetable
 
 import (
-	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 
-	"cloud.google.com/go/storage"
 	"github.com/labstack/echo"
 
 	"github.com/PuerkitoBio/goquery"
@@ -20,6 +17,7 @@ import (
 const (
 	url      = "https://ohmitetudo-bus.jorudan.biz/diagrampoledtl"
 	cacheExt = ".json"
+	bucket   = "analog-subset-179214.appspot.com"
 )
 
 var tdList = []string{".column_day1_t2", ".column_day2_t2", ".column_day3_t2"}
@@ -48,16 +46,13 @@ func ScrapeTimeTable(c echo.Context) error {
 	fr := c.QueryParam("fr")
 	dgmpl := c.QueryParam("dgmpl")
 	clientHash := c.QueryParam("hash")
-	filePath := fr + dgmpl + cacheExt
-	cacheHash, err := md5HashFromFile(filePath)
+	fileName := fr + dgmpl + cacheExt
+	data, err := fetchFromCloudStorage(fileName)
 	if err == nil {
-		if clientHash == cacheHash {
-			file, err := os.Open(filePath)
-			if err == nil {
-				bytedata, err := ioutil.ReadAll(file)
-				if err == nil {
-					return c.JSONBlob(http.StatusOK, bytedata)
-				}
+		cacheHash, err := md5HashFromData(data)
+		if err == nil {
+			if clientHash == cacheHash {
+				return c.JSONBlob(http.StatusOK, data)
 			}
 		}
 	}
@@ -67,42 +62,12 @@ func ScrapeTimeTable(c echo.Context) error {
 		return err
 	}
 	c.Echo().Logger.Debug("Successfully scrape from " + fullURL)
-	err = saveCache(timeTable, filePath)
+	err = saveCache(timeTable, fileName)
 	if err != nil {
 		return err
 	}
-	c.Echo().Logger.Debug("Successfully save file " + filePath)
+	c.Echo().Logger.Debug("Successfully save file " + fileName)
 	return c.JSON(http.StatusOK, timeTable)
-}
-
-func connectCloudStorage(data []byte, fileName string) {
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		fmt.Println("Client create error:", err)
-	}
-	bkt := client.Bucket("analog-subset-179214.appspot.com")
-	attrs, err := bkt.Attrs(ctx)
-	if err != nil {
-		fmt.Println("Error attrs: ", err)
-	}
-	fmt.Printf("bucket %s created at %s, is located in %s with storage class %s\n", attrs.Name, attrs.Created, attrs.Location, attrs.StorageClass)
-	obj := bkt.Object(fileName)
-	w := obj.NewWriter(ctx)
-	if _, err := w.Write(data); err != nil {
-		fmt.Println("Write error: ", err)
-	}
-	if err := w.Close(); err != nil {
-		fmt.Println("writer close error: ", err)
-	}
-	r, err := obj.NewReader(ctx)
-	if err != nil {
-		fmt.Println("Reader create error: ", err)
-	}
-	defer r.Close()
-	if _, err = io.Copy(os.Stdout, r); err != nil {
-		fmt.Println("read error:", err)
-	}
 }
 
 func saveCache(data interface{}, fileName string) error {
@@ -110,7 +75,10 @@ func saveCache(data interface{}, fileName string) error {
 	if err != nil {
 		return err
 	}
-	connectCloudStorage(jsonData, fileName)
+	err = saveToCloudStorage(jsonData, fileName)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -181,19 +149,27 @@ func scrapeTimeInfo(doc *goquery.Document) timeTable {
 	return timeTable
 }
 
-func md5HashFromFile(filePath string) (string, error) {
-	// file, err := os.Open(filePath)
-	// if err != nil {
-	// return "", err
-	// }
-	// defer file.Close()
-	// hash := md5.New()
-	// if _, err := io.Copy(hash, file); err != nil {
-	// 	return "", err
-	// }
-	// hashByte := hash.Sum(nil)
-	// hashString := hex.EncodeToString(hashByte)
-	// fmt.Println(hashString)
-	// return hashString, nil
-	return "hogehogehugahuga", nil
+// md5HashFromFile fileNameで示されるfileからmd5でハッシュ値を算出する
+func md5HashFromFile(fileName string) (string, error) {
+	data, err := fetchFromCloudStorage(fileName)
+	if err != nil {
+		return "", err
+	}
+	hashString, err := md5HashFromData(data)
+	if err != nil {
+		return "", err
+	}
+	return hashString, nil
+}
+
+// md5HashFromData byteデータからハッシュ値を算出する
+func md5HashFromData(data []byte) (string, error) {
+	hash := md5.New()
+	if _, err := hash.Write(data); err != nil {
+		return "", err
+	}
+	hashByte := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashByte)
+	fmt.Println(hashString)
+	return hashString, nil
 }
