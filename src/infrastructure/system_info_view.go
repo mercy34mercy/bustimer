82a60484559568586d565c4e90ca0b11f77e1ef6
shun-shun123 <shunsuke15vb@gmail.com>
2020-10-22 02:11:32 +0900

    SystemInfoの動作チェックのため500を返す

--- src/infrastructure/system_info_view.go
+++ src/infrastructure/system_info_view.go
@@ -7,9 +7,9 @@ import (
 
 func SystemInfoRequest(c Context) error {
 	systemInfo := domain.SystemInfo{
-		Status: http.StatusOK,
-		Message: "",
-		Time: "",
+		Status: http.StatusInternalServerError,
+		Message: "只今サーバでエラーが発生しており、原因を究明中です。ご不便をお掛けしてしまい申し訳ありません。",
+		Time: "06:00ごろ復旧予定です。",
 	}
 	return c.Response("SystemInfoRequest", http.StatusOK, systemInfo)
 }
