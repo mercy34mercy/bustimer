aeb787d460743f79b7510cc919b3a8edb6c854c6
天知 <shunsuke.kmkz0617@gmail.com>
2021-03-23 18:14:17 +0900

    Headerを設定

--- src/infrastructure/approach_info_fetcher.go
+++ src/infrastructure/approach_info_fetcher.go
@@ -73,8 +73,16 @@ func (fetcher ApproachInfoFetcher) FetchApproachInfos(approachInfoUrl string) do
 	// 返り値で返す変数を初期化
 	approachInfos := domain.CreateApproachInfos()
 
+	var accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+	var userAgent = "Busdes! server"
+
 	// ページの情報を取得する
-	resp, err := http.Get(approachInfoUrl)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", approachInfoUrl, nil)
+	// この辺りのHeaderを設定しないと403が返された
+	req.Header.Add("accept", accept)
+	req.Header.Add("User-Agent", userAgent)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Printf("Http/GET failed to %v because of %v", approachInfoUrl, err)
 		return approachInfos
